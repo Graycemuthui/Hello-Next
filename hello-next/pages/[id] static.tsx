dba337d679b44c493840facaeb2804c2e3b6400f
Grayce Muthui <graycemuthui20@gmail.com>
2023-01-02 17:42:11 +0300

    Render the page using server side

---
+++ hello-next/pages/[id] static.tsx
@@ -0,0 +1,49 @@
+import { Character, GetCharacterResults } from "../types";
+import Image from "next/image";
+import imageLoader from "../imageLoader";
+
+function CharacterPage({ character }: { character: Character }) {
+  return (
+    <div>
+      <h1>{character.name}</h1>
+      <Image
+        loader={imageLoader}
+        unoptimized
+        src={character.image}
+        alt={character.name}
+        width="200"
+        height="200"
+      />
+    </div>
+  );
+}
+
+// Using static generation: Client-side data fetching which means that some parts of the page can be rendered entirely by client side JS
+// both getStaticProps and getStaticPaths are used together
+export async function getStaticPaths() {
+  const res = await fetch("http://rickandmortyapi.com/api/character");
+  const { results }: GetCharacterResults = await res.json();
+
+  return {
+    paths: results.map((character) => {
+      return { params: { id: String(character.id) } };
+    }),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }: { params: { id: string } }) {
+  const res = await fetch(
+    `http://rickandmortyapi.com/api/character/${params.id}`
+  );
+
+  const character = await res.json();
+
+  return {
+    props: {
+      character,
+    },
+  };
+}
+
+export default CharacterPage;
--- hello-next/pages/[id].tsx
+++ hello-next/pages/[id].tsx
@@ -1,11 +1,19 @@
-import { Character, GetCharacterResults } from "../types";
+import { GetServerSideProps } from "next";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import imageLoader from "../imageLoader";
+import { Character } from "../types";
+import styles from "../../styles/Home.module.css";
 
+// This is the server-side rendering version of the page
 function CharacterPage({ character }: { character: Character }) {
+  // used to access the router component
+  const router = useRouter();
+
   return (
-    <div>
+    <div className={styles.container}>
       <h1>{character.name}</h1>
+
       <Image
         loader={imageLoader}
         unoptimized
@@ -18,25 +26,10 @@ function CharacterPage({ character }: { character: Character }) {
   );
 }
 
-// Using static generation: Client-side data fetching which means that some parts of the page can be rendered entirely by client side JS
-// both getStaticProps and getStaticPaths are used together
-export async function getStaticPaths() {
-  const res = await fetch("http://rickandmortyapi.com/api/character");
-  const { results }: GetCharacterResults = await res.json();
-
-  return {
-    paths: results.map((character) => {
-      return { params: { id: String(character.id) } };
-    }),
-    fallback: false,
-  };
-}
-
-export async function getStaticProps({ params }: { params: { id: string } }) {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   const res = await fetch(
-    `http://rickandmortyapi.com/api/character/${params.id}`
+    `https://rickandmortyapi.com/api/character/${context.query.id}`
   );
-
   const character = await res.json();
 
   return {
@@ -44,6 +37,6 @@ export async function getStaticProps({ params }: { params: { id: string } }) {
       character,
     },
   };
-}
+};
 
 export default CharacterPage;
